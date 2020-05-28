4c4c33ed59048b5400847d8e7653b3659b439e16
jczaplew <john@gaiagps.com>
2020-05-28 15:38:24 -0500

    Simplify

--- addElevation.js
+++ addElevation.js
@@ -1,15 +1,12 @@
 const {coordEach} = require('@turf/meta');
 
 module.exports = (geojson, elevationProvider) => {
-    const errors = []
+    let error = undefined;
     coordEach(geojson, coords => {
-        const [error, elevation] = elevationProvider.getElevation([coords[1], coords[0]]);
-        if (error) {
-            errors.push(error)
-            return;
-        };
+        const [elevationError, elevation] = elevationProvider.getElevation([coords[1], coords[0]]);
+        error = elevationError
         coords[2] = elevation;
     });
-    if (errors.length) return [errors[0]];
-    return [undefined, geojson];
+
+    return [error, geojson];
 }
