50de5ea67e6f4934da7636c98957a89e2285d75e
John J Czaplewski <john@gaiagps.com>
2020-06-05 12:20:00 -0500

    Don't throw errors; configure NO_DATA (#9)

--- addElevation.js
+++ addElevation.js
@@ -1,12 +1,10 @@
 const {coordEach} = require('@turf/meta');
 
 module.exports = (geojson, elevationProvider) => {
-    let error = undefined;
     coordEach(geojson, coords => {
-        const [elevationError, elevation] = elevationProvider.getElevation([coords[1], coords[0]]);
-        error = elevationError
+        const [, elevation] = elevationProvider.getElevation([coords[1], coords[0]]);
         coords[2] = elevation;
     });
 
-    return [error, geojson];
+    return [undefined, geojson];
 }
