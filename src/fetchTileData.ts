81598ba01531f3cb573246040cc11c7ad8b3d55e
Benjamin Black <bblack@outsideinc.com>
2024-04-11 14:46:57 -0600

    log LRU stats every 5 mins

--- src/fetchTileData.ts
+++ src/fetchTileData.ts
@@ -16,20 +16,47 @@ export const BAD_TILE = Symbol();
 
 // ...could signal other Fetcher error conditions, e.g. bad permissions, network failure, etc.
 
-const cache = new LRUCache<bigint, Buffer>({
-    sizeCalculation: (buffer) => buffer.length, // bytes
-    maxSize: parseInt(process.env.MAX_LRU_SIZE!), // 500 MB, specified in bytes because `sizeCalculation` returns bytes
-});
-
 const pending = new Map<bigint, Promise<Buffer>>();
 
 const missing = new Set<bigint>();
+
+let hits = 0;
+let misses = 0;
+let evictions = 0;
+
+const cache = new LRUCache<bigint, Buffer>({
+    sizeCalculation: (buffer) => buffer.length, // bytes
+    maxSize: parseInt(process.env.MAX_LRU_SIZE!), // should be same unit as `sizeCalculation`
+    dispose: () => {
+        ++evictions;
+    },
+});
+
+setInterval(
+    () => {
+        const accesses = hits + misses;
+        const hitRate = accesses > 0 ? hits / accesses : 0;
+        const evictionRate = accesses > 0 ? evictions / accesses : 0;
+        const fillPercentage = cache.calculatedSize / cache.maxSize;
+
+        fastify.log.info({lruCacheStats: {fillPercentage, hitRate, evictionRate}});
+
+        // Reset stats each interval. Maybe @TODO: total stats?
+        hits = misses = evictions = 0;
+    },
+    1_000 * 60 * 5, // 5 mins
+);
 
 export async function fetchTileData<T extends unknown[]>(fetcher: Fetcher<T>, ...args: T) {
     const hash = fnv1a(JSON.stringify(args));
 
     // If the LRU cache has it, return it
-    if (cache.has(hash)) return cache.get(hash)!;
+    if (cache.has(hash)) {
+        ++hits;
+        return cache.get(hash)!;
+    } else {
+        ++misses;
+    }
 
     // If we're currently fetching it, return the promise
     if (pending.has(hash)) return pending.get(hash)!;
