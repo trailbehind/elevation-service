9a4707cb873b1312f8fe401036665d7ec1b9a3c8
Benjamin Black <bblack@outsideinc.com>
2024-04-15 09:12:30 -0600

    fix test

--- tests/elevation/getElevation.test.js
+++ tests/elevation/getElevation.test.js
@@ -21,19 +21,10 @@ test('getting elevation for a point', async () => {
     assert.strictEqual(elevation >= 4399, true);
 });
 
-it('returns 0 elevation and logs when a tile is missing', async () => {
-    // mock.method(server, 'log');
-
+it('returns 0 elevation when a tile is missing', async () => {
     const elevation = await getElevation([0, 0]);
 
     assert.strictEqual(elevation, 0);
-
-    // Not exactly bulletproof, but testing for a string match is more fragile
-    // assert.strictEqual(console.log.mock.calls.length, 1);
-    // assert.strictEqual(console.log.mock.calls[0].arguments.length, 1);
-    // assert.strictEqual(typeof console.log.mock.calls[0].arguments[0], 'string');
-
-    // mock.reset();
 });
 
 after(async () => {
